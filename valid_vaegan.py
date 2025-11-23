212a590a68d31c7600208e2c5aa9c0eadd2be8f1
tufimtseva <tatyana.ufimtseva@gmail.com>
2025-11-22 21:53:11 -0500

    set  num_workers=0 in dataloader

--- valid_vaegan.py
+++ valid_vaegan.py
@@ -118,7 +118,7 @@ def validation(local_rank, d_configs, p_configs, num_sampling, logger):
     valid_dataset = BDDataset(set_type='valid', noisy=False, noise_level=0, **dataset_args)
     valid_loader = DataLoader(valid_dataset,
                               batch_size=1,
-                              num_workers=d_configs['num_workers'],
+                              num_workers=0,
                               pin_memory=True)
 
     evaluate(d_model, p_model, valid_loader, local_rank, num_sampling, logger, valid_dataset.sigma, denoiser)
