d605e3f7b042f96e77ddec63320664182eb28cd7
shreya <shreyashree00015@gmail.com>
2025-11-23 19:33:37 -0500

    save the images

--- valid_vaegan.py
+++ valid_vaegan.py
@@ -17,13 +17,18 @@ from model.bicyclegan.global_model import GuidePredictor as GP
 from tqdm import tqdm
 from torch.distributed.elastic.multiprocessing.errors import record
 from data.dataset import BAistPP as BDDataset
+
 loss_fn_alex = lpips.LPIPS(net='alex').to('cuda:0')
 import torchvision.utils as vutils
 
 import os
 
-SAVE_DIR = "reconstruction_results"
+SAVE_DIR = "saved_output"
 os.makedirs(SAVE_DIR, exist_ok=True)
+# Create subdirectories for before/after
+os.makedirs(join(SAVE_DIR, "before_processing"), exist_ok=True)
+os.makedirs(join(SAVE_DIR, "after_processing"), exist_ok=True)
+
 
 def init_seeds(seed=0):
     random.seed(seed)
@@ -33,7 +38,6 @@ def init_seeds(seed=0):
 
 
 def validation(local_rank, d_configs, p_configs, num_sampling, logger):
-    # Preparation and backup
     torch.backends.cudnn.benchmark = True
 
     # model init
@@ -43,7 +47,7 @@ def validation(local_rank, d_configs, p_configs, num_sampling, logger):
     # dataset init
     dataset_args = d_configs['dataset_args']
 
-    for noise_level in [0, 5, 10, 20, 30, 40, 50]:
+    for noise_level in [0, 5, 10, 20]:
         print(f"[INFO] Testing with noise_level={noise_level}")
 
         dataset_args_override = dataset_args.copy()
@@ -56,20 +60,10 @@ def validation(local_rank, d_configs, p_configs, num_sampling, logger):
                                   num_workers=d_configs['num_workers'],
                                   pin_memory=True)
 
-        # CHANGED: Call evaluate inside the loop
         evaluate(d_model, p_model, valid_loader, local_rank, num_sampling, logger, valid_dataset.sigma)
 
-    # CHANGED: Close logger after all noise levels
     logger.close()
 
-
-    # valid_dataset = BDDataset(set_type='valid', noisy=True, noise_level=0, **dataset_args)
-    # valid_loader = DataLoader(valid_dataset,
-    #                           batch_size=1,
-    #                           num_workers=d_configs['num_workers'],
-    #                           pin_memory=True)
-
-    # evaluate(d_model, p_model, valid_loader, local_rank, num_sampling, logger, valid_dataset.sigma)
 
 @torch.no_grad()
 def evaluate(d_model, p_model, valid_loader, local_rank, num_sampling, logger, sigma):
@@ -84,12 +78,20 @@ def evaluate(d_model, p_model, valid_loader, local_rank, num_sampling, logger, s
     lpips_meter_better = AverageMeter()
     time_stamp = time.time()
 
-    # One epoch validation
+    # Number of sequences to save (save first 5 sequences)
+    NUM_SEQUENCES_TO_SAVE = 5
+
     for i, tensor in enumerate(tqdm(valid_loader, total=len(valid_loader))):
         tensor['inp'] = tensor['inp'].to(device)  # (b, 1, 3, h, w)
         tensor['trend'] = torch.zeros_like(tensor['inp'])[:, :, :2]
         tensor['gt'] = tensor['gt'].to(device)  # (b, num_gts, 3, h, w)
         b, num_gts, c, h, w = tensor['gt'].shape
+
+        # === SAVE INPUT (BEFORE) ===
+        if i < NUM_SEQUENCES_TO_SAVE:
+            input_img = tensor['inp'][0, 0].cpu()  # (3, h, w)
+            input_save_path = join(SAVE_DIR, "before_processing", f"sigma_{sigma}_seq{i}_input.png")
+            vutils.save_image(input_img / 255.0, input_save_path)  # Normalize to [0, 1] for saving
 
         psnr_vals = []
         ssim_vals = []
@@ -107,21 +109,24 @@ def evaluate(d_model, p_model, valid_loader, local_rank, num_sampling, logger, s
             tensor['trend'] = out_tensor['pred_trends'].to(device)
             tensor['trend'] = F.interpolate(tensor['trend'], size=(h, w))[None]
             out_tensor = d_model.update(inp_tensor=tensor, training=False)
-            pred_imgs = out_tensor['pred_imgs']  # pred_imgs shape (b, num_gts, 3, h, w)
-            gt_imgs = out_tensor['gt_imgs']  # gt_imgs shape (b, num_gts, 3, h, w)
+            pred_imgs = out_tensor['pred_imgs']
+            gt_imgs = out_tensor['gt_imgs']
             pred_imgs = pred_imgs.reshape(num_gts * b, c, h, w)
 
-
-            if i < 5:
-                # pred_imgs has shape (num_gts * b, 3, H, W)
+            # === SAVE OUTPUT FRAMES (AFTER) ===
+            if i < NUM_SEQUENCES_TO_SAVE:
                 for j in range(pred_imgs.shape[0]):
-                    save_path = os.path.join(SAVE_DIR, f"sigma_{sigma}seq{i}_sample{j}.png")
-                    # bring back to 0–1
-                    img = pred_imgs[j]
-                           # .clamp(0, 255) / 255.0
-                    vutils.save_image(img, save_path)
+                    frame_save_path = join(SAVE_DIR, "after_processing", f"sigma_{sigma}_seq{i}_frame{j}.png")
+                    frame_img = pred_imgs[j].cpu()
+                    vutils.save_image(frame_img / 255.0, frame_save_path)  # Normalize to [0, 1]
 
-            # Record loss and metrics
+                # Also save GT frames for reference
+                gt_imgs_save = gt_imgs.reshape(num_gts * b, c, h, w)
+                for j in range(gt_imgs_save.shape[0]):
+                    gt_save_path = join(SAVE_DIR, "after_processing", f"sigma_{sigma}_seq{i}_gt{j}.png")
+                    gt_img = gt_imgs_save[j].cpu()
+                    vutils.save_image(gt_img / 255.0, gt_save_path)
+
             pred_imgs = pred_imgs.to(device)
             gt_imgs = gt_imgs.to(device)
             pred_imgs_rev = torch.flip(pred_imgs, dims=[1, ])
@@ -151,21 +156,78 @@ def evaluate(d_model, p_model, valid_loader, local_rank, num_sampling, logger, s
         psnr_meter_better.update(max(psnr_vals_better), num_gts * b)
         ssim_meter_better.update(max(ssim_vals_better), num_gts * b)
         lpips_meter_better.update(min(lpips_vals_better), num_gts * b)
-        # print('{}/{}'.format(i + 1, len(valid_loader)), psnr_meter.avg, ssim_meter.avg, lpips_meter.avg)
-        # print('{}/{}'.format(i + 1, len(valid_loader)), psnr_meter_better.avg, ssim_meter_better.avg,
-        #       lpips_meter_better.avg)
 
     # Ending of validation
     eval_time_interval = time.time() - time_stamp
     msg = 'eval time: {} sec, psnr: {:.5f}, ssim: {:.5f}, lpips: {:.4f}'.format(
         eval_time_interval, psnr_meter.avg, ssim_meter.avg, lpips_meter.avg
     )
-    logger(msg, prefix=f'[valid σ={sigma}]')  # CHANGED: Added sigma to prefix
+    logger(msg, prefix=f'[valid σ={sigma}]')
     msg = 'eval time: {:.4f} sec, psnr: {:.4f}, ssim: {:.4f}, lpips: {:.4f}'.format(
         eval_time_interval, psnr_meter_better.avg, ssim_meter_better.avg, lpips_meter_better.avg
     )
     logger(msg, prefix=f'[valid max. σ={sigma}]')
-#
+
+    print(f"[INFO] Saved images for σ={sigma} in {SAVE_DIR}")
+
+
+@record
+def main():
+    # load args & configs
+    parser = ArgumentParser(description='Guidance prediction & Blur Decomposition')
+    parser.add_argument('--local_rank', default=0, type=int, help='local rank')
+    parser.add_argument('--log_dir', default='logs', help='path of log')
+    parser.add_argument('--log_name', default='valid', help='log name')
+    parser.add_argument('--predictor_resume_dir', help='path of log', required=True)
+    parser.add_argument('--decomposer_resume_dir', help='path of log', required=True)
+    parser.add_argument('--data_dir', nargs='+', required=True)
+    parser.add_argument('--num_iters', type=int, default=1, help='number of iters')
+    parser.add_argument('-ns', '--num_sampling', type=int, default=1, help='number of sampling times')
+    parser.add_argument('--verbose', action='store_true', help='whether to print out logs')
+    args = parser.parse_args()
+    num_sampling = args.num_sampling
+
+    # Para for decomposer
+    decomposer_config = join(args.decomposer_resume_dir, 'cfg.yaml')
+    with open(decomposer_config) as f:
+        d_configs = yaml.full_load(f)
+    d_configs['resume_dir'] = args.decomposer_resume_dir
+    d_configs['num_iterations'] = args.num_iters
+
+    # Para for predictor
+    predictor_config = join(args.predictor_resume_dir, 'cfg.yaml')
+    with open(predictor_config, 'rt', encoding='utf8') as f:
+        p_configs = yaml.full_load(f)
+    p_configs['bicyclegan_args']['checkpoints_dir'] = args.predictor_resume_dir
+    p_configs['bicyclegan_args']['isTrain'] = False
+    p_configs['bicyclegan_args']['epoch'] = 'latest'
+    p_configs['bicyclegan_args']['no_encode'] = False
+
+    is_gen_blur = True
+    for root_dir in d_configs['dataset_args']['root_dir']:
+        if 'b-aist++' in root_dir:
+            is_gen_blur = False
+
+        d_configs['dataset_args']['aug_args']['valid']['image'] = {
+            'NearBBoxResizedSafeCrop': {'max_ratio': 0, 'height': 192, 'width': 160}
+        }
+    d_configs['dataset_args']['root_dir'] = args.data_dir
+    d_configs['dataset_args']['use_trend'] = False
+
+    logger = Logger(file_path=join(args.log_dir, '{}.txt'.format(args.log_name)), verbose=args.verbose)
+    dist.init_process_group(backend="nccl")
+    torch.cuda.set_device(args.local_rank)
+    rank = dist.get_rank()
+    init_seeds(seed=rank)
+
+    validation(local_rank=args.local_rank, d_configs=d_configs, p_configs=p_configs, num_sampling=num_sampling,
+               logger=logger)
+    dist.destroy_process_group()
+
+
+if __name__ == '__main__':
+    main()
+
 # @torch.no_grad()
 # def evaluate(d_model, p_model, valid_loader, local_rank, num_sampling, logger, sigma):
 #     # Preparation
@@ -340,82 +402,3 @@ def evaluate(d_model, p_model, valid_loader, local_rank, num_sampling, logger, s
 #
 # if __name__ == '__main__':
 #     main()
-
-@record
-def main():
-    # load args & configs
-    parser = ArgumentParser(description='Guidance prediction & Blur Decomposition')
-    parser.add_argument('--local_rank', default=0, type=int, help='local rank')
-    parser.add_argument('--log_dir', default='logs', help='path of log')
-    parser.add_argument('--log_name', default='valid', help='log name')
-    parser.add_argument('--predictor_resume_dir', help='path of log', required=True)
-    parser.add_argument('--decomposer_resume_dir', help='path of log', required=True)
-    parser.add_argument('--data_dir', nargs='+', required=True)
-    parser.add_argument('--num_iters', type=int, default=1, help='number of iters')
-    parser.add_argument('-ns', '--num_sampling', type=int, default=1, help='number of sampling times')
-    parser.add_argument('--verbose', action='store_true', help='whether to print out logs')
-    args = parser.parse_args()
-    num_sampling = args.num_sampling
-
-    # Para for decomposer
-    decomposer_config = join(args.decomposer_resume_dir, 'cfg.yaml')
-    with open(decomposer_config) as f:
-        d_configs = yaml.full_load(f)
-    d_configs['resume_dir'] = args.decomposer_resume_dir
-    d_configs['num_iterations'] = args.num_iters
-
-    # Para for predictor
-    predictor_config = join(args.predictor_resume_dir, 'cfg.yaml')
-    with open(predictor_config, 'rt', encoding='utf8') as f:
-        p_configs = yaml.full_load(f)
-    p_configs['bicyclegan_args']['checkpoints_dir'] = args.predictor_resume_dir
-    p_configs['bicyclegan_args']['isTrain'] = False
-    p_configs['bicyclegan_args']['epoch'] = 'latest'
-    p_configs['bicyclegan_args']['no_encode'] = False
-
-    # Import blur decomposition dataset
-    is_gen_blur = True
-    for root_dir in d_configs['dataset_args']['root_dir']:
-        if 'b-aist++' in root_dir:
-            is_gen_blur = False
-    # if is_gen_blur:
-    #     from data.dataset import GenBlur as BDDataset
-    #
-    #     d_configs['dataset_args']['aug_args']['valid']['image'] = {}
-    # else:
-
-
-        d_configs['dataset_args']['aug_args']['valid']['image'] = {
-            'NearBBoxResizedSafeCrop': {'max_ratio': 0, 'height': 192, 'width': 160}
-        }
-    d_configs['dataset_args']['root_dir'] = args.data_dir
-    d_configs['dataset_args']['use_trend'] = False
-
-    # Logger init
-    logger = Logger(file_path=join(args.log_dir, '{}.txt'.format(args.log_name)), verbose=args.verbose)
-
-    # DDP init
-    # if torch.cuda.device_count() > 1:
-    #     dist.init_process_group(backend="nccl")
-    #     print("Distributed initialized with NCCL")
-    #     rank = dist.get_rank()
-    # else:
-    #     print("Single GPU detected — skipping distributed initialization")
-    #     rank = 0
-    dist.init_process_group(backend="nccl")
-    torch.cuda.set_device(args.local_rank)
-    rank = dist.get_rank()
-    init_seeds(seed=rank)
-
-    # Training model
-    validation(local_rank=args.local_rank, d_configs=d_configs, p_configs=p_configs, num_sampling=num_sampling, logger=logger)
-
-    # Tear down the process group
-    # dist.destroy_process_group()
-    # if torch.cuda.device_count() > 1:
-    dist.destroy_process_group()
-
-
-
-if __name__ == '__main__':
-    main()
